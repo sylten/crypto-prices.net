436521fc364e7fd8ff37efbe5e1880eb4067ffe8
jonas <jonas@jshakansson.se>
2021-04-25 15:49:41 +0200

    abbreviate mcap

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
           <td>{coin.current_price}</td>
           <td className="change no-mobile">{coin.price_change_24h.toFixed(3)}</td>
           <td className="change">{coin.price_change_percentage_24h.toFixed(3)}%</td>
-          <td className="no-mobile">{coin.market_cap}</td>
+          <td className="no-mobile">{abbr(coin.market_cap, 2)}</td>
           <td className="no-mobile">{coin.ath}</td>
         </tr>
       ))
@@ -59,4 +59,48 @@ export default function Home() {
       Powered by <a href="http://coingecko.com/" target="_blank" className="coingecko">CoinGecko</a>
     </div>
   )
+}
+
+// from https://stackoverflow.com/questions/2685911/is-there-a-way-to-round-numbers-into-a-reader-friendly-format-e-g-1-1k
+export function abbr(number, decPlaces) {
+  var isNegative = number < 0;
+  number = Math.abs(number);
+
+  // 2 decimal places => 100, 3 => 1000, etc
+  decPlaces = Math.pow(10,decPlaces);
+
+  // Enumerate number abbreviations
+  var abbrev = [ "K", "M", "B", "T" ];
+
+  // Go through the array backwards, so we do the largest first
+  for (var i=abbrev.length-1; i>=0; i--) {
+
+      // Convert array index to "1000", "1000000", etc
+      var size = Math.pow(10,(i+1)*3);
+
+      // If the number is bigger or equal do the abbreviation
+      if(size <= number) {
+           // Here, we multiply by decPlaces, round, and then divide by decPlaces.
+           // This gives us nice rounding to a particular decimal place.
+           number = Math.round(number*decPlaces/size)/decPlaces;
+
+           // Handle special case where we round up to the next abbreviation
+           if((number === 1000) && (i < abbrev.length - 1)) {
+               number = 1;
+               i++;
+           }
+
+           // Add the letter for the abbreviation
+           number += abbrev[i];
+
+           // We are done... stop
+           break;
+      }
+  }
+
+  if (isNegative) {
+      return '-' + number;
+  }
+
+  return number;
 }
